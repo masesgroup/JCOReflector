57b7c0886c09f4ff84226e4179c6548b03955ca4
Mario Mastrodicasa <mario.mastrodicasa@masesgroup.com>
2021-08-25 16:36:14 +0200

    Added basic test for system.Object

---
+++ netreflected-tests/scala/src/main/scala/mscorlib/HelloLock.scala
@@ -0,0 +1,51 @@
+/*
+ *  MIT License
+ *
+ *  Copyright (c) 2021 MASES s.r.l.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+package mscorlib
+
+import org.mases.jcobridge.netreflection.JCOReflector
+import system.threading.Monitor
+
+object HelloLock {
+  def main(args: Array[String]): Unit = {
+    JCOReflector.setCommandLineArgs(args)
+    try {
+      val `object` = new Object
+      val lockTaken = false
+      try {
+        Monitor.Enter(`object`, lockTaken)
+        if (lockTaken) System.out.println("Lock taken")
+        else {
+          System.out.println("Failed to acquire lock")
+          System.exit(1)
+        }
+      } finally Monitor.Exit(`object`)
+      System.out.println("Exiting with success")
+      System.exit(0)
+    } catch {
+      case tre: Throwable =>
+        tre.printStackTrace()
+    }
+  }
+}

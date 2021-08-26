1bc0c264177a3260ead7c9e4a781cebbaa5b16a9
Mario Mastrodicasa <mario.mastrodicasa@masesgroup.com>
2021-08-26 19:30:15 +0200

    Retrigger CI/CD chain

--- netreflected/src/net461/mscorlib_version_4.0.0.0_culture_neutral_publickeytoken_b77a5c561934e089/system/__ComObject.java
+++ netreflected/src/net461/mscorlib_version_4.0.0.0_culture_neutral_publickeytoken_b77a5c561934e089/system/__ComObject.java
@@ -42,7 +42,6 @@ import system.MarshalByRefObject;
 
 
 
-
 /**
  * The base .NET class managing System.__ComObject, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089.
  * <p>

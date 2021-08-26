df70fe7826f3885ca741a5dd5451a7833c6704b8
Mario Mastrodicasa <mario.mastrodicasa@masesgroup.com>
2021-08-26 19:27:19 +0200

    Trigger CI/CD chain

--- netreflected/src/net461/mscorlib_version_4.0.0.0_culture_neutral_publickeytoken_b77a5c561934e089/system/__ComObject.java
+++ netreflected/src/net461/mscorlib_version_4.0.0.0_culture_neutral_publickeytoken_b77a5c561934e089/system/__ComObject.java
@@ -41,6 +41,8 @@ import java.util.ArrayList;
 import system.MarshalByRefObject;
 
 
+
+
 /**
  * The base .NET class managing System.__ComObject, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089.
  * <p>

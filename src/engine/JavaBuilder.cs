0a0b553e1c0f8463aeb2099ebbbfcd7d498dce61
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2022-03-14 19:04:15 +0100

    #93: fix

--- src/engine/JavaBuilder.cs
+++ src/engine/JavaBuilder.cs
@@ -225,53 +225,50 @@ namespace MASES.JCOReflectorEngine
             {
                 POMBuilderEventArgs args = o as POMBuilderEventArgs;
 
-                await Task.Run(() =>
+                if (!Path.IsPathRooted(args.SourceFolder))
                 {
-                    if (!Path.IsPathRooted(args.SourceFolder))
-                    {
-                        args.SourceFolder = Path.Combine(args.RootFolder, args.SourceFolder);
-                    }
+                    args.SourceFolder = Path.Combine(args.RootFolder, args.SourceFolder);
+                }
 
-                    if (args.POMType == POMType.Frameworks)
-                    {
-                        Const.FileNameAndDirectory.CreateJCOBridgeZip(args.RootFolder);
-                    }
+                if (args.POMType == POMType.Frameworks)
+                {
+                    Const.FileNameAndDirectory.CreateJCOBridgeZip(args.RootFolder);
+                }
 
-                    var srcRootFolder = args.SplitFolderByAssembly ? Path.Combine(args.SourceFolder, Const.FileNameAndDirectory.SourceDirectory) : args.SourceFolder;
-                    var assembliesToUse = (args.AssembliesToUse == null) ? JobManager.CreateFolderList(srcRootFolder, false) : args.AssembliesToUse;
-                    StringBuilder sb = new StringBuilder();
-                    foreach (var item in assembliesToUse)
-                    {
-                        sb.AppendFormat(Const.POM.POM_JCOREFLECTOR_SOURCE_PLACEHOLDER, item);
-                        sb.AppendLine();
-                    }
-                    var sourceFlders = sb.ToString();
-                    //sourceFlders = sourceFlders.Remove(sourceFlders.LastIndexOf(','));
-                    sourceFlders = sourceFlders.Replace('\\', '/');
-                    string jcoPomTemplate = string.Empty;
-                    if (args.POMType == POMType.Extension && !string.IsNullOrEmpty(args.POMFileName))
-                    {
-                        jcoPomTemplate = File.ReadAllText(args.POMFileName);
-                    }
-                    else
-                    {
-                        jcoPomTemplate = Const.Templates.GetTemplate((args.POMType == POMType.Frameworks) ? Const.Templates.POMJCOReflector : Const.Templates.POMExtension);
-                    }
+                var srcRootFolder = args.SplitFolderByAssembly ? Path.Combine(args.SourceFolder, Const.FileNameAndDirectory.SourceDirectory) : args.SourceFolder;
+                var assembliesToUse = (args.AssembliesToUse == null) ? JobManager.CreateFolderList(srcRootFolder, false) : args.AssembliesToUse;
+                StringBuilder sb = new StringBuilder();
+                foreach (var item in assembliesToUse)
+                {
+                    sb.AppendFormat(Const.POM.POM_JCOREFLECTOR_SOURCE_PLACEHOLDER, item);
+                    sb.AppendLine();
+                }
+                var sourceFlders = sb.ToString();
+                //sourceFlders = sourceFlders.Remove(sourceFlders.LastIndexOf(','));
+                sourceFlders = sourceFlders.Replace('\\', '/');
+                string jcoPomTemplate = string.Empty;
+                if (args.POMType == POMType.Extension && !string.IsNullOrEmpty(args.POMFileName))
+                {
+                    jcoPomTemplate = File.ReadAllText(args.POMFileName);
+                }
+                else
+                {
+                    jcoPomTemplate = Const.Templates.GetTemplate((args.POMType == POMType.Frameworks) ? Const.Templates.POMJCOReflector : Const.Templates.POMExtension);
+                }
 
-                    var jcoPom = jcoPomTemplate.Replace(Const.POM.POM_VERSION_PLACEHOLDER, args.POMVersion + ((args.POMStagingType == POMStagingType.Snapshot) ? Const.POM.POM_VERSION_SNAPSHOT : string.Empty))
-                                               .Replace(Const.POM.POM_RUNTIME_PLACEHOLDER, Const.Framework.RuntimeFolder)
-                                               .Replace(Const.POM.POM_JDK_TARGET, ((int)args.JDKTarget).ToString())
-                                               .Replace(Const.POM.POM_SOURCEDIRECTORIES_PLACEHOLDER, sourceFlders)
-                                               .Replace(Const.POM.POM_ARTIFACTID_PLACEHOLDER, args.POMArtifactId)
-                                               .Replace(Const.POM.POM_NAME_PLACEHOLDER, args.POMName)
-                                               .Replace(Const.POM.POM_DESCRIPTION_PLACEHOLDER, args.POMDescription)
-                                               .Replace(Const.POM.POM_JCOREFLECTOR_VERSION_PLACEHOLDER, Const.ReflectorVersion)
-                                               .Replace(Const.POM.POM_ADDITIONAL_DEPENDENCIES_PLACEHOLDER, args.POMAdditionalDependencies ?? string.Empty);
+                var jcoPom = jcoPomTemplate.Replace(Const.POM.POM_VERSION_PLACEHOLDER, args.POMVersion + ((args.POMStagingType == POMStagingType.Snapshot) ? Const.POM.POM_VERSION_SNAPSHOT : string.Empty))
+                                           .Replace(Const.POM.POM_RUNTIME_PLACEHOLDER, Const.Framework.RuntimeFolder)
+                                           .Replace(Const.POM.POM_JDK_TARGET, ((int)args.JDKTarget).ToString())
+                                           .Replace(Const.POM.POM_SOURCEDIRECTORIES_PLACEHOLDER, sourceFlders)
+                                           .Replace(Const.POM.POM_ARTIFACTID_PLACEHOLDER, args.POMArtifactId)
+                                           .Replace(Const.POM.POM_NAME_PLACEHOLDER, args.POMName)
+                                           .Replace(Const.POM.POM_DESCRIPTION_PLACEHOLDER, args.POMDescription)
+                                           .Replace(Const.POM.POM_JCOREFLECTOR_VERSION_PLACEHOLDER, Const.ReflectorVersion)
+                                           .Replace(Const.POM.POM_ADDITIONAL_DEPENDENCIES_PLACEHOLDER, args.POMAdditionalDependencies ?? string.Empty);
 
-                    var fileName = Path.Combine(srcRootFolder, string.Format("{0}.xml", Const.Framework.RuntimeFolder));
-                    File.WriteAllText(fileName, jcoPom);
-                    reportStr = string.Format("{0} POM created in {1}.", fileName, DateTime.Now - dtStart);
-                });
+                var fileName = Path.Combine(srcRootFolder, string.Format("{0}.xml", Const.Framework.RuntimeFolder));
+                File.WriteAllText(fileName, jcoPom);
+                reportStr = string.Format("{0} POM created in {1}.", fileName, DateTime.Now - dtStart);
             }
             catch (OperationCanceledException ex)
             {
@@ -307,12 +304,9 @@ namespace MASES.JCOReflectorEngine
             {
                 POMBuilderEventArgs args = o as POMBuilderEventArgs;
 
-                await Task.Run(() =>
-                {
-                    var jcoPomTemplate = Const.Templates.GetTemplate(Const.Templates.POMExtension);
-                    File.WriteAllText(args.POMFileName, jcoPomTemplate);
-                    reportStr = string.Format("{0} POM created in {1}.", args.POMFileName, DateTime.Now - dtStart);
-                });
+                var jcoPomTemplate = Const.Templates.GetTemplate(Const.Templates.POMExtension);
+                File.WriteAllText(args.POMFileName, jcoPomTemplate);
+                reportStr = string.Format("{0} POM created in {1}.", args.POMFileName, DateTime.Now - dtStart);
             }
             catch (OperationCanceledException ex)
             {

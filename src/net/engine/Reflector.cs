7d7c586104680a99109c8681dce9c2ff0cefbc8c
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2026-09-17 15:39:25 +0200

    Use old method since IEnumerable and IEnumerator are implemented from base engine

--- src/net/engine/Reflector.cs
+++ src/net/engine/Reflector.cs
@@ -863,9 +863,17 @@ namespace MASES.JCOReflector.Engine
                 withInheritance = true;
                 foreach (var inter in implementableInterfaces)
                 {
-                    if (inter == typeof(IEnumerable)) packageBaseClass = Const.SpecialNames.NetIEnumerable + Const.SpecialNames.ImplementationTrailer;
-                    if (inter == typeof(IEnumerator)) packageBaseClass = Const.SpecialNames.NetIEnumerator + Const.SpecialNames.ImplementationTrailer;
-                    packageBaseInterface += string.Format(", {0}", inter.ToPackageName() + "." + inter.GetJavaClassName(item.Assembly));
+                    if (inter == typeof(IEnumerable))
+                    {
+                        packageBaseClass = Const.SpecialNames.NetIEnumerable + Const.SpecialNames.ImplementationTrailer;
+                        packageBaseInterface += string.Format(", {0}", inter.Name);
+                    }
+                    else if (inter == typeof(IEnumerator)) 
+                    {
+                        packageBaseClass = Const.SpecialNames.NetIEnumerator + Const.SpecialNames.ImplementationTrailer;
+                        packageBaseInterface += string.Format(", {0}", inter.Name);
+                    }
+                    else packageBaseInterface += string.Format(", {0}", inter.ToPackageName() + "." + inter.GetJavaClassName(item.Assembly));
                     imports.Add(inter);
                 }
             }
